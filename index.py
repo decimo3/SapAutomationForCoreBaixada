240115970b4e194200978ae14d537aa6ac153669
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-04-28 10:35:34 -0300

    testes com correspondências humanizadas

--- index.py
+++ index.py
@@ -18,19 +18,20 @@ class index:
       if self.texto != self.ultimo_texto:
         self.ultimo_texto = self.texto
         argumentos = self.texto.split(' ')
-        if re.search("^\:*", argumentos[0]):
-          print(argumentos[0])
-          if argumentos[0] == ":d":
-            self.resposta = self.sape.debito(argumentos[1])
-            self.resposta = self.xlsx.debito(self.resposta)
-            self.whats.responde(self.resposta)
-          elif argumentos[0] == ":r":
-            self.resposta = self.sape.relatorio(argumentos[1])
-            self.whats.responde(self.resposta)
-          elif argumentos[0] == ":l":
-            self.resposta = self.sape.leiturista(argumentos[1])
-            self.whats.responde(self.resposta)
-          elif argumentos[0] == ":h":
-            self.resposta = self.sape.historico(argumentos[1])
-            self.whats.responde(self.resposta)
+        # if re.search("débito", self.texto):
+        # if re.search("^\:*", argumentos[0]):
+        # print(argumentos[0])
+        if re.search("débito", self.texto):
+          self.resposta = self.sape.debito(argumentos[1])
+          self.resposta = self.xlsx.escrever(self.resposta)
+          self.whats.responde(self.resposta)
+        if argumentos[0] == ":r":
+          self.resposta = self.sape.relatorio(argumentos[1])
+          self.whats.responde(self.resposta)
+        if argumentos[0] == ":l":
+          self.resposta = self.sape.leiturista(argumentos[1])
+          self.whats.responde(self.resposta)
+        if argumentos[0] == ":h":
+          self.resposta = self.sape.historico(argumentos[1])
+          self.whats.responde(self.resposta)
 robo = index()
