6f810035998bc01abf11a0dfe7998c802589fa66
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-06-23 15:21:04 -0300

    CFG: automatic build

---
+++ build.bat
@@ -0,0 +1,15 @@
+REM @echo OFF
+REM cls
+REM .\venv\Scripts\Activate
+pyinstaller --onefile .\\src\\sap.py
+pyinstaller --onefile .\\src\\img.py
+cd ..
+copy .\Automacao\fileDialog.vbs .\telbot
+copy .\Automacao\dist\* .\telbot
+cd .\telbot
+dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true
+cd ..
+copy .\telbot\bin\Debug\net6.0\win-x64\publish\* %USERPROFILE%\MestreRuan\
+cd .\Automacao
+venv\Scripts\deactivate.bat
+pause
