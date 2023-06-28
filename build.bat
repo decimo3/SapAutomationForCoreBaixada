002709695471f137427459808285f3a273c9256e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-06-28 14:53:03 -0300

    Commented out lines of activation and disabling of the virtual environment that were interrupting the script

--- build.bat
+++ build.bat
@@ -11,5 +11,5 @@ dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true
 cd ..
 copy .\telbot\bin\Debug\net6.0\win-x64\publish\* %USERPROFILE%\MestreRuan\
 cd .\Automacao
-venv\Scripts\deactivate.bat
+REM venv\Scripts\deactivate.bat
 pause
