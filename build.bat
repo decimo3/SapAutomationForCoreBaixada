a571107cedd048932a8fdf104ddf6af2dc9b7e1d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-02-22 14:39:39 -0300

    Undoed changes. Because the script needs to be in the current project folder

--- build.bat
+++ build.bat
@@ -1,22 +1,20 @@
 @echo OFF
 if defined VIRTUAL_ENV (
-pyinstaller --onefile sap.py
-pyinstaller --onefile img.py
-pyinstaller --onefile etc.py
-rm sap.db
-sqlite3 sap.db < sap.sql
-cd ..
+pyinstaller --onefile .\\src\\sap.py
+pyinstaller --onefile .\\src\\img.py
+del .\\src\\sap.db
+sqlite3 .\\src\\sap.db < .\\src\\sap.sql
 cd ..
 mkdir tmp
 copy .\SapAutomationForCoreBaixada\src\sap.db tmp
 copy .\SapAutomationForCoreBaixada\src\sap.conf tmp
+copy .\SapAutomationForCoreBaixada\dist\sap.exe tmp
+copy .\SapAutomationForCoreBaixada\dist\img.exe tmp
 copy .\SapAutomationForCoreBaixada\src\erroDialog.vbs tmp
 copy .\SapAutomationForCoreBaixada\src\fileDialog.vbs tmp
-copy .\SapAutomationForCoreBaixada\dist\* tmp
 cd .\TelegramBotForFieldTeamHelper
 dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true --output ..\tmp\
 cd ..
-robocopy tmp %USERPROFILE%\MestreRuan\ /XF database.db /XF .env
 7z u -x!database.db -x!sap.conf -x!.env mestreruan.zip .\tmp\*
 REM https://superuser.com/questions/1654994/how-to-copy-folder-structure-but-exclude-certain-files-in-windows
 cd .\SapAutomationForCoreBaixada
