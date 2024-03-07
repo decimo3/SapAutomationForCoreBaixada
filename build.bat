f0872c058ee83747a01c661ebcf83ee2d03a0232
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-03-07 12:05:18 -0300

    Added instruction to copy the announcement file to the output folder

--- build.bat
+++ build.bat
@@ -12,6 +12,7 @@ copy .\SapAutomationForCoreBaixada\src\sap.db tmp
 copy .\SapAutomationForCoreBaixada\src\sap.conf tmp
 copy .\SapAutomationForCoreBaixada\dist\sap.exe tmp
 copy .\SapAutomationForCoreBaixada\dist\img.exe tmp
+copy .\SapAutomationForCoreBaixada\comunicado.txt tmp
 copy .\SapAutomationForCoreBaixada\src\erroDialog.vbs tmp
 copy .\SapAutomationForCoreBaixada\src\fileDialog.vbs tmp
 cd .\TelegramBotForFieldTeamHelper
