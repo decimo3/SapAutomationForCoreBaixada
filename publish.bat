1d47240545d068b0289f206f56a6207a6faa3e46
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-05-21 16:00:11 -0300

    Update 'publish.bat'

--- publish.bat
+++ publish.bat
@@ -2,12 +2,12 @@
 git diff --exit-code comunicado.txt > nul
 if %ERRORLEVEL% equ 0 (
   echo Updating the 'comunicado.txt' file is necessary to publish
-  exit
+  exit /B
 )
 set "UPDATE_FOLDER=\\192.168.10.213\chatbot\"
 if not exist %UPDATE_FOLDER% (
   echo The shared folder path '%UPDATE_FOLDER%' was not found!
-  exit
+  exit /B
 )
 call build.bat
 cd ..
@@ -15,6 +15,3 @@ for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set da
 set "datestamp=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%"
 7z u -x!database.db -x!.env -x!sap.conf -x!ofs.conf .\releases\%datestamp%.zip .\tmp\*
 cd .\SapAutomationForCoreBaixada
-pause
-git add comunicado.txt
-git commit -m "Update 'comunicado.txt'"
