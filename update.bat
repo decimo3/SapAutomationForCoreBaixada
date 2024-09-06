8fe2f91e028ce374d526e56b2dbf864ceb8190ee
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-09-06 11:28:33 -0300

    Fixed update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,6 @@
 @echo off
 call build.bat
 cd ..
-robocopy tmp %USERPROFILE%\MestreRuan\ /XF database.db /XF .env /XF sap.conf /XF ofs.conf
+robocopy tmp\net7.0\win-x64\publish tmp\ /S
+robocopy tmp %USERPROFILE%\MestreRuan\ /S /XD net7.0 /XF database.db /XF .env /XF sap.conf /XF ofs.conf
 cd .\SapAutomationForCoreBaixada
