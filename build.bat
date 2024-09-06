9c03fbb94dcafcbae59b9686cddf3717f0256433
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-09-06 09:01:41 -0300

    Removed publish arguments because it is already specified on project configuration file

--- build.bat
+++ build.bat
@@ -15,20 +15,11 @@ copy .\SapAutomationForCoreBaixada\dist\img.exe tmp
 copy .\SapAutomationForCoreBaixada\comunicado.txt tmp
 copy .\SapAutomationForCoreBaixada\src\erroDialog.vbs tmp
 copy .\SapAutomationForCoreBaixada\src\fileDialog.vbs tmp
-cd .\TelegramBotForFieldTeamHelper
 for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set datetime=%%a
 set "datestamp=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%"
-echo %datestamp% > version
-dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true --output ..\tmp\
-cd ..
-cd .\monitoring-fieldteam\src
-dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true --output ..\..\tmp\
-cd ..
-cd ..
-cd .\SapAutomationForWeb
-dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true --output ..\tmp\
-cd ..
-cd .\loc_zone_finder
-dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true --output ..\tmp\
-cd ..
+echo %datestamp% > .\TelegramBotForFieldTeamHelper\version
+dotnet publish .\TelegramBotForFieldTeamHelper\telbot.csproj
+dotnet publish .\monitoring-fieldteam\src\ofs.csproj
+dotnet publish .\SapAutomationForWeb\prl.csproj
+dotnet publish .\loc_zone_finder\gps.csproj
 cd .\SapAutomationForCoreBaixada
