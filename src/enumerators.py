e232222bc12224d912c18301d2021feddc93d98a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-01-16 16:01:35 -0300

    Swapped Enum for IntEnum to get Enum's index number

--- src/enumerators.py
+++ src/enumerators.py
@@ -1,53 +1,53 @@
 ''' Hodule that holds enumerates '''
-from enum import Enum
+from enum import IntEnum
 
-class DESTAQUES(Enum):
+class DESTAQUES(IntEnum):
   ''' Class to hold enumerate with colors to csv highlights '''
   AUSENTE = 0
   AMARELO = 1
   VERMELHO = 2
   VERDE = 3
 
-class ES32_FLAGS(Enum):
+class ES32_FLAGS(IntEnum):
   ONLY_INST = 0
   GET_CENTER = 1
   GET_METER = 2
   ENTER_CONSUMO = 3
 
-class IW53_FLAGS(Enum):
+class IW53_FLAGS(IntEnum):
   GET_INST = 0
 
-class ZMED89_FLAGS(Enum):
+class ZMED89_FLAGS(IntEnum):
   TIME_ORDER = 0
   SEQ_ORDER = 1
   TELEMEDIDO = 2
 
-class ZARC140_FLAGS(Enum):
+class ZARC140_FLAGS(IntEnum):
   GET_PENDING = 0
   GET_RENOTICE = 1
 
-class ES61_FLAGS(Enum):
+class ES61_FLAGS(IntEnum):
   ENTER_ENTER = 0
   SKIPT_ENTER = 1
   ENTER_LIGACAO = 2
   SKIPT_ENTER_LIGACAO_ENTER = 3
 
-class ES57_FLAGS(Enum):
+class ES57_FLAGS(IntEnum):
   ENTER_ENTER = 0
   SKIPT_ENTER = 1
   ENTER_LOGRADOURO = 2
   SKIPT_ENTER_LOGRADOURO_ENTER = 3
 
-class ZMED95_FLAGS(Enum):
+class ZMED95_FLAGS(IntEnum):
   ENTER_ENTER = 0
   SKIPT_ENTER = 1
   CHECK_PASIVE = 2
   CHECK_VALUES = 3
 
-class BP_FLAGS(Enum):
+class BP_FLAGS(IntEnum):
   GET_PHONES = 0
   GET_DOCS = 1
 
-class IQ03_FLAGS(Enum):
+class IQ03_FLAGS(IntEnum):
   ONLY_INST = 0
   READ_REPORT = 1
