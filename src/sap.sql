bb2aefeb423cc5e65b034f266337d8e8a5b40139
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-27 17:00:07 -0300

    removed unuseds table relatorio_tipo

--- src/sap.sql
+++ src/sap.sql
@@ -719,11 +719,6 @@ INSERT INTO depara (tipo, de, para) VALUES
 ('material_codigo','392396','POLO P/MEDICAO MT 15KV BCPS11'),
 ('material_codigo','811566','TRANSFORMADOR CORRENTE 145KV'),
 ('material_codigo','811567','TRANSFORMADOR CORRENTE 145KV');
--- Entering values ​​for report params
-INSERT INTO depara (tipo, de, para) VALUES
-('relatorio_tipo','RELIGA','B1,BL,BR'),
-('relatorio_tipo','LIDE','B8,BN,BS,BV'),
-('relatorio_tipo','ANEXO','B5,BA,BC,BS');
 -- Entering values ​​for report filters
 INSERT INTO depara (tipo, de, para) VALUES
 ('relatorio_filtro', 'RELIGA', 'ORTI,ORIS,ORRS'),
